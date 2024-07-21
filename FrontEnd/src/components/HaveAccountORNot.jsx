f93b9d00c4d9e84e4d212a7cd62c5b8b2b8cbc19
TEJAS KAWARKHE <tejas.kawarkhe555@gmail.com>
2024-07-21 16:48:15 +0530

    HaveAccountORNot

--- FrontEnd/src/components/HaveAccountORNot.jsx
+++ FrontEnd/src/components/HaveAccountORNot.jsx
@@ -5,6 +5,7 @@ function HaveAccountORNot() {
     <div className="border border-gray-500 p-5 text-center w-80 mx-auto mt-2 rounded shadow gap-2">
       Don&apos;t have an account? <Link to="#" className="text-blue-500 hover:underline">Sign up</Link>
     </div>
+    
   )
 }
 
