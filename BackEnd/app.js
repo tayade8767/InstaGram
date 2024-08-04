a4aee9a3fcaad5bb2fdab61061601a5c4193c9e1
akash <akashtayade5668@gmail.com>
2024-08-04 18:09:47 +0530

    adding modelm start

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,8 +10,12 @@ app.use(cors({
 
 app.use(express.json({limit: "32kb"}))
 app.use(express.urlencoded({ extended:true,limit:"32kb" }))
+app.use(express.static("public"))
 app.use(cookieParser())
 
-import userRouter from "./routes/";
+
+
+
+
 
 export {app}
--- BackEnd/db/index.js
+++ BackEnd/db/index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constant.js";
+
+import { DB_NAME } from "../constant.js";
+
 const connectDB = async () =>{
     try{
         const connctionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
