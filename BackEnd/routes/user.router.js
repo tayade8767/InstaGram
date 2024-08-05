aa9daa7a22427acf56a74fb6c4c25f0159628f80
TEJAS KAWARKHE <tejas.kawarkhe555@gmail.com>
2024-08-05 23:35:16 +0530

    myallchanges

---
+++ BackEnd/routes/user.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+
+
+import {upload} from "../middlewares/multer.middleware.js";
+import {VerifyUserJWT} from "../middlewares/auth.middleware.js";
+
+const router =Router();
+
+router.post("/register",)
+
+router.post("/login").post()
