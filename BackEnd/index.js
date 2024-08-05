705a907ca96aa074f479612f74e00418c75ad30d
akash <akashtayade5668@gmail.com>
2024-08-05 23:46:32 +0530

    chanhes added

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,7 +11,6 @@ connectDB()
     app.listen(process.env.PORT||3000,()=>{
         console.log(`Server is running on port ${process.env.PORT}`)
     })
-    .catch((err)=>{
+}).catch((err)=>{
     console.error("mongodb server connection failed");
-    })
 })
